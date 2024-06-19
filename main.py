b86be913a92d58eff7c2c46e84d618eb22c4f2e4
Saanav Somani <somani19@cs.washington.edu>
2024-06-19 12:27:27 -0700

    Completed part 2 (path parameters) tutorial.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -14,4 +15,29 @@ async def post():
 async def put():
   return {"message": "Welcome to put!"}
 
+@app.get("/users")
+async def list_users():
+  return {"message": "List of users"}
 
+#  want this specific endpoint first before the dynamic endpoint
+#  (order of routes matter)
+@app.get("users/current_user")
+async def get_current_user():
+  return {"message":"This is the current user"}
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+  return {"user_id": user_id}
+
+class SportEnum(str, Enum):
+  basketball = "basketball"
+  football = "football"
+  baseball = "baseball"
+
+@app.get("/sports/{sport}")
+async def get_sport(sport: SportEnum):
+  if sport == SportEnum.basketball:
+    return {"sport": sport, "GOAT": "Michael Jordan"}
+  elif sport == SportEnum.football: # can check value - sport == "football"
+    return {"sport": sport, "GOAT": "Tom Brady"}
+  return {"sport": sport, "GOAT": "Babe Ruth"}
